b61f6f2b73ebc69c08e917a05c7c25ba9b9d96a9
Thosine-01 <lawaltosine01@gmail.com>
2022-05-22 00:22:05 +0100

    tis is the third commit

--- index.js
+++ index.js
@@ -6,3 +6,34 @@ menu.addEventListener('click', function(){
     menu.classList.toggle('is_active');
     menuList.classList.toggle('active');
 });
+
+
+var index = 0;
+var slides = document.querySelectorAll(".slides");
+var dot = document.querySelectorAll(".dot");
+
+function changeSlide(){
+
+  if(index<0){
+    index = slides.length-1;
+  }
+  
+  if(index>slides.length-1){
+    index = 0;
+  }
+  
+  for(let i=0;i<slides.length;i++){
+    slides[i].style.display = "none";
+    dot[i].classList.remove("activate");
+  }
+  
+  slides[index].style.display= "block";
+  dot[index].classList.add("activate");
+  
+  index++;
+  
+  setTimeout(changeSlide,3000);
+  
+}
+
+changeSlide();
